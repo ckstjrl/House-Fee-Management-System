e2e50650be1d07234c7b8de18f80caf0cc0a4d57
ckstjrl <qkrckstjr7876@gmail.com>
2026-01-11 18:09:06 +0900

    feat(view): design dashboard, record input, and item management UI

---
+++ Views/DashBoardView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using ManagementHouseFee.ViewModels; // 뷰모델 참조
+
+namespace ManagementHouseFee.Views
+{
+    public partial class DashboardView : UserControl
+    {
+        public DashboardView()
+        {
+            InitializeComponent();
+
+            // 뷰모델 연결 (DataContext 설정)
+
+            this.DataContext = new DashboardViewModel();
+        }
+    }
+}
